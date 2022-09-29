4fa00a99510dffaf879a8d76ae30e5a3046df850
jojeongbin <chojb2004@gmail.com>
2022-09-29 21:15:59 +0900

    Road

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,11 +5,21 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    enum driveType
+    {
+        FRONTDRIVE,
+        REARDRIVE,
+        ALLDRIVE
+    }
+    [SerializeField]driveType drive;
+
     public WheelCollider[] wheels = new WheelCollider[4];
     GameObject[] wheelMesh = new GameObject[4];
 
     public float power = 100f;
     public float rot = 45f;
+    public float downForceValue;
+    public float radius = 6;
 
     Rigidbody rb;
     InputManager IM;
@@ -31,12 +41,102 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate() 
     {
-        for (int i = 0; i < wheels.Length; i ++)
-            wheels[i].motorTorque = IM.vertical * power;
-        
-        for (int i = 0; i < 2; i ++)
-            wheels[i].steerAngle = IM.horizontal * rot;
-        WheelPosAndAni();   
+        // 전륜 구동일 때
+        if (drive == driveType.ALLDRIVE)
+        {
+            for (int i = 0; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = IM.vertical * (power/4);
+            }
+            
+            if (IM.vertical == 0)	// 전진 중이 아닐 때
+            {
+                for (int i = 0; i < wheels.Length; i++)
+                {
+                    wheels[i].brakeTorque = power/4;
+                }
+            }   
+            else	// 키를 눌렀을 때
+            {
+                for (int i = 0; i < wheels.Length; i++)
+                {
+                    wheels[i].brakeTorque = 0; // 브레이크 해제
+                }
+            }
+        }   
+        else if (drive == driveType.REARDRIVE)	// 후륜구동일 때
+        {
+            // 뒷바퀴에만.
+            for (int i = 2; i < wheels.Length; i++)
+            {
+                wheels[i].motorTorque = IM.vertical * (power / 2);
+            }
+
+            if (IM.vertical == 0)
+            {
+                for (int i = 0; i < wheels.Length; i++)
+                {
+                    wheels[i].brakeTorque = power/2;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < wheels.Length; i++)
+                {
+                    wheels[i].brakeTorque = 0;
+                }
+            }
+        }
+        else	// 전륜 구동일 때
+        {	// 앞바퀴에만
+            for (int i = 0; i < 2; i++)
+            {
+                wheels[i].motorTorque = IM.vertical * (power / 2);
+            }
+            if (IM.vertical == 0)
+            {
+                for (int i = 0; i < wheels.Length; i++)
+                {
+                    wheels[i].brakeTorque = power/2;
+                }
+            }
+            else
+            {
+                for (int i = 0; i < wheels.Length; i++)
+                {
+                        wheels[i].brakeTorque = 0;
+                }
+            }
+        }
+
+        SteerVehicle();
+        WheelPosAndAni();
+        AddDownForce();
+    }
+
+    private void SteerVehicle()
+    {
+        if (Input.GetAxis("Horizontal") > 0)
+        {   // rear tracks size is set to 1.5f          wheel base has been set to 2.55f
+            wheels[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(2.55f / (radius + (1.5f / 2))) * IM.horizontal;
+            wheels[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(2.55f / (radius - (1.5f / 2))) * IM.horizontal;
+        }
+        else if (Input.GetAxis("Horizontal") < 0)
+        {
+            wheels[0].steerAngle = Mathf.Rad2Deg * Mathf.Atan(2.55f / (radius - (1.5f / 2))) * IM.horizontal;
+            wheels[1].steerAngle = Mathf.Rad2Deg * Mathf.Atan(2.55f / (radius + (1.5f / 2))) * IM.horizontal;
+            // transform.Rotate(Vector3.up * steerHelping)
+        }
+        else
+        {
+            wheels[0].steerAngle = 0;
+            wheels[1].steerAngle = 0;
+        }
+    }
+
+    private void AddDownForce()
+    {
+        rb.AddForce(-transform.up * downForceValue * rb.velocity.magnitude);
     }
 
     private void WheelPosAndAni()
